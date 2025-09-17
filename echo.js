5de9fa56223599311b853bf6e25ce64679ba38ca
vijayrathee8-dot <vijay.rathee8@gmail.com>
2025-09-17 15:01:05 +0530

    Add echo command

---
+++ echo.js
@@ -0,0 +1,1 @@
+module.exports = { name: "echo", description: "Repeat your message", execute(api, message, args) { if (!args.length) return api.sendMessage("Usage: !echo [text]", message.threadID); api.sendMessage(args.join(" "), message.threadID); } };
